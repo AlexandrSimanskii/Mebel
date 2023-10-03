bcefa6692e76dda1794d89d193fd5ee98b2e4ea1
AlexandrSimanskii <119415846+AlexandrSimanskii@users.noreply.github.com>
2023-10-03 21:07:10 +0200

    Updates

---
+++ assets/Login-d41c367c.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-13fa8a7a.js";import{F as o}from"./Form-1cdec4a4.js";import"./index.esm-f3316ed0.js";const e=()=>r.jsx(r.Fragment,{children:r.jsx(o,{})});export{e as default};
--- assets/NotFound-e2167c5b.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-d10f9b5f.js";const o=()=>s.jsx("div",{className:"notFound",children:s.jsx("div",{className:"container",children:"Такой страницы не существует"})});export{o as default};
